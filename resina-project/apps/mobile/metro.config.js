7adee71cd09aa41a947f4a68c3a75077f8a61d66
MushroomStake <winterloudrel@gmail.com>
2026-04-19 05:22:54 +0800

    final fix for mobile apk

--- resina-project/apps/mobile/metro.config.js
+++ resina-project/apps/mobile/metro.config.js
@@ -1,6 +1,22 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+// Support hoisted Expo packages while forcing React resolution to mobile-local copies.
+config.watchFolders = [workspaceRoot];
+config.resolver.nodeModulesPaths = [
+	path.resolve(projectRoot, "node_modules"),
+	path.resolve(workspaceRoot, "node_modules"),
+];
+config.resolver.disableHierarchicalLookup = true;
+config.resolver.extraNodeModules = {
+	react: path.resolve(projectRoot, "node_modules/react"),
+	"react-dom": path.resolve(projectRoot, "node_modules/react-dom"),
+	"react-native": path.resolve(projectRoot, "node_modules/react-native"),
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
